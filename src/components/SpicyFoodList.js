57ea64f88678fefbcfec5b6ecf2f2cfa94fdb335
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-09-24 00:35:46 +0300

    Completed Assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,22 +3,70 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All")
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  })
+
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    // console.log(newFood);
+    const updatedFoods = [...foods, newFood];
+    setFoods(updatedFoods)
+    // console.log(foods)
+  
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleClick(id) {
+    const updatedFoods = foods.filter(food => food.id !== id);
+    setFoods(updatedFoods);
+
+    const newFoodsArray = foods.map((food) => {
+      if(food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        }}
+         else {
+          return food;
+         }
+    });
+    setFoods(newFoodsArray);
+  }
+        
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value)
+  }
+
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{foodList}</ul>
+      <ul>
+
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
+
+        {foodList}
+        </ul>
     </div>
   );
 }
